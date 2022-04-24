1994c96386a1213425f1ea8ec41c18deb4a11107
Yash Aggarwal <yashaggarwal0009@gmail.com>
2022-04-24 22:43:35 +0530

    Add log to AES algo

--- src/main/java/AESEncrypt.java
+++ src/main/java/AESEncrypt.java
@@ -1,3 +1,4 @@
+import utils.CryptoUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -5,17 +6,8 @@ import javax.crypto.spec.GCMParameterSpec;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import utils.CryptoUtils;
 
-/**
- * AES-GCM inputs - 12 bytes IV, need the same IV and secret keys for encryption and decryption.
- * <p>
- * The output consist of iv, encrypted content, and auth tag in the following format:
- * output = byte[] {i i i c c c c c c ...}
- * <p>
- * i = IV bytes
- * c = content bytes (encrypted content, auth tag)
- */
+
 public class AESEncrypt {
 
     private static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
@@ -25,7 +17,6 @@ public class AESEncrypt {
 
     private static final Charset UTF_8 = StandardCharsets.UTF_8;
 
-    // AES-GCM needs GCMParameterSpec
     public static byte[] encrypt(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
 
         Cipher cipher = Cipher.getInstance(ENCRYPT_ALGO);
@@ -35,7 +26,6 @@ public class AESEncrypt {
 
     }
 
-    // prefix IV length + IV bytes to cipher text
     public static byte[] encryptWithPrefixIV(byte[] pText, SecretKey secret, byte[] iv) throws Exception {
 
         byte[] cipherText = encrypt(pText, secret, iv);
@@ -63,7 +53,6 @@ public class AESEncrypt {
 
         byte[] iv = new byte[IV_LENGTH_BYTE];
         bb.get(iv);
-        //bb.get(iv, 0, iv.length);
 
         byte[] cipherText = new byte[bb.remaining()];
         bb.get(cipherText);
@@ -78,16 +67,13 @@ public class AESEncrypt {
         String OUTPUT_FORMAT = "%-30s:%s";
 
         String pText = "Hello World AES-GCM, Welcome to Cryptography!";
-
-        // encrypt and decrypt need the same key.
-        // get AES 256 bits (32 bytes) key
+        long startTime = System.currentTimeMillis();
         SecretKey secretKey = CryptoUtils.getAESKey(AES_KEY_BIT);
 
-        // encrypt and decrypt need the same IV.
-        // AES-GCM needs IV 96-bit (12 bytes)
         byte[] iv = CryptoUtils.getRandomNonce(IV_LENGTH_BYTE);
 
         byte[] encryptedText = AESEncrypt.encryptWithPrefixIV(pText.getBytes(UTF_8), secretKey, iv);
+        System.out.println(System.currentTimeMillis() - startTime + " is the time taken for AES");
 
         System.out.println("\n------ AES GCM Encryption ------");
         System.out.println(String.format(OUTPUT_FORMAT, "Input (plain text)", pText));
@@ -95,16 +81,6 @@ public class AESEncrypt {
         System.out.println(String.format(OUTPUT_FORMAT, "IV  (hex)", CryptoUtils.hex(iv)));
         System.out.println(String.format(OUTPUT_FORMAT, "Encrypted (hex) ", CryptoUtils.hex(encryptedText)));
         System.out.println(String.format(OUTPUT_FORMAT, "Encrypted (hex) (block = 16)", CryptoUtils.hexWithBlockSize(encryptedText, 16)));
-
-        System.out.println("\n------ AES GCM Decryption ------");
-        System.out.println(String.format(OUTPUT_FORMAT, "Input (hex)", CryptoUtils.hex(encryptedText)));
-        System.out.println(String.format(OUTPUT_FORMAT, "Input (hex) (block = 16)", CryptoUtils.hexWithBlockSize(encryptedText, 16)));
-        System.out.println(String.format(OUTPUT_FORMAT, "Key (hex)", CryptoUtils.hex(secretKey.getEncoded())));
-
-        String decryptedText = AESEncrypt.decryptWithPrefixIV(encryptedText, secretKey);
-
-        System.out.println(String.format(OUTPUT_FORMAT, "Decrypted (plain text)", decryptedText));
-
     }
 
 }
